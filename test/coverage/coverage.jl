c8dff09837ecc57f3f9ef8d60a76b3520d740d75
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2022-03-10 21:42:01 +0100

    try submit codecov

--- test/coverage/coverage.jl
+++ test/coverage/coverage.jl
@@ -2,7 +2,7 @@ using Coverage
 
 cd(joinpath(@__DIR__, "..", "..")) do
     processed = process_folder()
-    # Codecov.submit_local(processed)
+    Codecov.submit_local(processed)
     covered_lines, total_lines = get_summary(processed)
     percentage = covered_lines / total_lines * 100
     println("($(percentage)%) covered")
